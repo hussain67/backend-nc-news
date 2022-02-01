e13256b122afbfbe10ebdfbf33d50b01ff94e2ae
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-02-01 11:26:45 +0000

    Refactored

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,12 @@ const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
   path: `${__dirname}/../../.env.${ENV}`
 });
+
 /*
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
+
 */
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
